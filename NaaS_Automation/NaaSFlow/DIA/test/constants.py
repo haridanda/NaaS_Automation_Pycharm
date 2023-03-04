d7ff6f640a7778f6daa392a5b0baf5d86d656f36
haridanda <harisaireddy.danda1@lumen.com>
2023-03-04 16:31:31 -0700

    Via YAML data load.

---
+++ NaaS_Automation/NaaSFlow/DIA/test/constants.py
@@ -0,0 +1,15 @@
+my_cookie = None
+#Uni_id = None
+path = "//Users//haridanda//PycharmProjects//act//NaaS_Automation//NaaSFlow//DIA//test//Sample1.xls"
+login = "/login"
+UNI_workflow = "/workflow_engine/startJobWithOptions/LNAAS_CREATE_UNI_SL_V1"
+skinny_workflow = "/workflow_engine/startJobWithOptions/Create_Resource_ASRI_AL"
+SL_workflow = "/workflow_engine/startJobWithOptions/LNAAS_CREATE_DIA_SERVICE_SL_V1"
+SL_D_workflow = "/workflow_engine/startJobWithOptions/LNAAS_DELETE_DIA_SERVICE_SL_V1"
+SL_DUNI_workflow = "/workflow_engine/startJobWithOptions/LNAAS_DELETE_UNI_SL_V1"
+mesh_path = "/inventory/v1/mesh/paths?aend=LABBRMCO&product=ETHERNET&numpaths=1&bandwidth=100&diversity=No&interface=Optical&protection=No&productType=simple_ethernet&lowLatency=No&dwdmOnly=No&metroLevel=2.0"
+workflow_status = "/workflow_engine/getJobShallow/"
+workflow_search = "/workflow_engine/tasks/search"
+sasi_asri = "/wrappers/asri/naming/generate"
+assign_ip ="/wrappers/nisws/ipAssign"
+get_asri ="/inventory/v1/asri/services?name="
